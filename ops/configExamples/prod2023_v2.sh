c708ff1a563e05ef6b8dff44742d271868135571
E-T-K <e-t-k@users.noreply.github.com>
2023-08-16 17:27:41 -0700

    Updates to dockerized data server
    - new config file prod2023_v2. designed for use with --network="host" in the
    docker run command - so it only broadcasts on 127.0.0.1 instead of
    0.0.0.0 .
    
    - designed for when the view server is hosted on hexcalc
    
    - also bugfix to checkProc so it loads the enviroment vars

---
+++ ops/configExamples/prod2023_v2.sh
@@ -0,0 +1,65 @@
+
+ROOT=/data
+#CERTS=$ROOT/certs
+
+export ADMIN_EMAIL=hexmap@ucsc.edu
+
+# Whether the server runs with nohup, or runs directly on your terminal.
+#"FORE" or "BACK"
+export BACK_OR_FOREGROUND=BACK
+
+# https certificate authority chain
+#export CA=$CERTS/chain.crt
+
+# https public certificate
+#export CERT=$CERTS/server.crt
+
+# Points to the map data that will be served.
+export DATA_ROOT=$ROOT/data
+
+# Logging debug level when 1, when 0 production logging level
+export DEBUG=0
+
+# Add DRL to the path.
+export DRLPATH=$HEXCALC/calc/DRL_bin
+
+# Controls amount of chatter on server output.
+export FLASK_DEBUG=0
+
+# Username under which the data server will run, for checking existence of
+# server processes.
+export HEX_UID=hexmap
+
+# User group to own server processes on protected ports.
+export HEX_GID=hexdocker
+
+# Port on which the server will listen.
+export PORT=4000
+
+# This path needs to be above or equal to your 'compute' dir in the
+# file hierarchy.
+export PYENV=$HEXCALC/../env
+
+# Path to the test data root. 
+export TEST_DATA_ROOT=$HEXCALC/tests/in/dataRoot
+
+# View servers allowed to edit maps. 128.114.198.36 = hexcalc.ucsc.edu 128.114.198.35 = hexmap.gi.ucsc.edu
+export VIEW_SERVER_ADDRS=128.114.198.36
+
+# Bookmarks are created by this view server.
+export VIEWER_URL=https://tumormap.ucsc.edu
+
+# IP socket and user view of server URL. When running in Docker without network="host", use 0.0.0.0:$PORT
+# Otherwise, use 127.0.0.1:$PORT
+export WWW_SOCKET=127.0.0.1:$PORT
+export DATA_SERVER=https://hexcalc.ucsc.edu
+
+# Path to store sqlite database needed for JobQueue
+# defaults to appCtx.hubPath + '/../computeDb' when not provided
+# Seems to not work right now; enable with caution
+#export DATABASE_PATH=$HEXCALC/../computeDb
+
+# If the python environment is present then open it up.
+if [ -e $PYENV/bin/activate ]; then
+    source $PYENV/bin/activate
+fi
